6b6344652c40fb1878397be5057139377f40c098
floor12 <primelifecoder@gmail.com>
2017-09-29 12:32:05 +0200

    Modal window control class added

---
+++ src/ModalWindow.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: floor12
+ * Date: 25.09.2017
+ * Time: 11:09
+ */
+
+namespace floor12\editmodal;
+
+
+use yii\helpers\Html;
+
+class ModalWindow
+{
+
+    const TYPE_INFO = 0;
+    const TYPE_OK = 1;
+    const TYPE_ERROR = 2;
+
+    private $_return;
+
+
+    public function hide()
+    {
+        $this->_return .= "hideFormModal();";
+        return $this;
+    }
+
+    public function info($content, $type)
+    {
+        $this->_return .= "info(\"{$content}\",{$type});";
+        return $this;
+    }
+
+
+    public function reloadContainer($container_name)
+    {
+        $this->_return .= "$.pjax.reload({container:\"{$container_name}\"});";
+        return $this;
+    }
+
+    public function run()
+    {
+        return Html::script($this->_return);
+    }
+}
