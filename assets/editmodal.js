dffb39b2500a91b6e5d247584d8112765a9daef9
floor12 <primelifecoder@gmail.com>
2018-04-17 18:26:30 +0200

    Фикс для модального окна в маке.

--- assets/editmodal.js
+++ assets/editmodal.js
@@ -126,10 +126,10 @@ function deleteItem(route, id) {
 
 function hideFormModal() {
     $('#modaledit-modal').modal('hide');
-    $('body').removeClass('modal-open'); //bugfix
-    $('.modal-backdrop').fadeOut(150, function () {
-        $('.modal-backdrop').remove();
-    }) //bugfix
+    $('body').removeClass('modal-open');                //bugfix
+    $('.modal-backdrop').fadeOut(150, function () {     //bugfix
+        $('.modal-backdrop').remove();                  //bugfix
+    })                                                  //bugfix
     $('#modaledit-modal div.modal-content').html("");
 }
 
